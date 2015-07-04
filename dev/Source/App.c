7f6019c71bb7bdc45a2402692bd52513fd257f13
Stuart Wheater <stuart.wheater@gmail.com>
2015-07-04 21:20:55 +0100

    Initial complete version

--- dev/Source/App.c
+++ dev/Source/App.c
@@ -2,12 +2,39 @@
  * Copyright (c) 2015, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
  */
 
+#define SIM_SCGC5       ((unsigned int*) 0x40048038)
+#define GPIO_PORTC_PCR5 ((unsigned int*) 0x4004B014)
+#define GPIO_PORTC_OR   ((unsigned int*) 0xF8000080)
+#define GPIO_PORTC_SOR  ((unsigned int*) 0xF8000084)
+#define GPIO_PORTC_COR  ((unsigned int*) 0xF8000088)
+#define GPIO_PORTC_TOR  ((unsigned int*) 0xF800008C)
+#define GPIO_PORTC_IR   ((unsigned int*) 0xF8000090)
+#define GPIO_PORTC_DR   ((unsigned int*) 0xF8000094)
+
+void setWord(volatile unsigned int* address, unsigned int value)
+{
+    *address = value;
+}
+
+unsigned int getWord(volatile unsigned int* address)
+{
+    return *address;
+}
+
 void setup(void)
 {
+    setWord(SIM_SCGC5, getWord(SIM_SCGC5) | (1 << 11));
+    setWord(GPIO_PORTC_PCR5, 0b00000000000000000000000100000000);
+    setWord(GPIO_PORTC_DR,   0b00000000000000000000000000100000);
 }
 
 void loop(void)
 {
+    volatile int index;
+
+    setWord(GPIO_PORTC_SOR, 0b00000000000000000000000000100000);
+    for (index = 0; index < 250000; index++) ;
+
+    setWord(GPIO_PORTC_COR, 0b00000000000000000000000000100000);
+    for (index = 0; index < 250000; index++) ;
 }
-
-
--- dev/Source/Init.s
+++ dev/Source/Init.s
@@ -4,12 +4,37 @@
 
 	.text
 
-start:
-	SETSP	=stack
+	.word	_stack		/* Initial Stack Pointer */
+	.word	_start		/* Reset */
+	.word	_dummy		/* NMI */
+	.word	_dummy		/* Hard Fault */
+	.word	_dummy		/* RESERVED */
+	.word	_dummy		/* RESERVED */
+	.word	_dummy		/* RESERVED */
+	.word	_dummy		/* RESERVED */
+	.word	_dummy		/* RESERVED */
+	.word	_dummy		/* RESERVED*/
+	.word	_dummy		/* RESERVED */
+	.word	_dummy		/* SVCall */
+	.word	_dummy		/* RESERVED */
+	.word	_dummy		/* RESERVED */
+	.word	_dummy		/* PendSV */
+	.word	_dummy		/* SysTick */
+	.word	_dummy		/* IRQ0 */
+	.word	_dummy		/* IRQ1 */
+	.word	_dummy		/* IRQ2 */
+	.word	_dummy		/* IRQ3 */
+
+	.thumb_func
+_start:
 	BL	setup
-label:
+_label:
 	BL	loop
-	B	label
+	B	_label
+
+	.thumb_func
+_dummy:
+	B	_dummy
 
 	.ltorg
 
@@ -17,6 +42,6 @@ label:
 
 	.align	4
 	.space	4096
-stack:
+_stack:
 
 	.end
