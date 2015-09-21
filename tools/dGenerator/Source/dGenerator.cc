fd4954b2a15e455123f20b9f0ae94b463a6a5fe2
Stuart Wheater <stuart.wheater@gmail.com>
2015-09-21 20:00:21 +0000

    Initial 'dGenerator'

---
+++ tools/dGenerator/Source/dGenerator.cc
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2015, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
+ */
+
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+    return EXIT_SUCCESS;
+}
